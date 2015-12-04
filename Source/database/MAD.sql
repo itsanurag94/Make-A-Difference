b89f12e492afa8cca34f9df45e9eb119398825fe
AravindAshok <aravindasokcn@gmail.com>
2015-12-05 01:26:32 +0530

    Updated Database schema in MAD.sql; Updated code (partially) to match new schema

--- Source/database/MAD.sql
+++ Source/database/MAD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 03, 2015 at 06:29 PM
+-- Generation Time: Dec 05, 2015 at 01:23 AM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `MAD`
 --
-CREATE DATABASE IF NOT EXISTS `MAD` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `MAD`;
 
 -- --------------------------------------------------------
 
@@ -29,21 +27,30 @@ USE `MAD`;
 --
 
 CREATE TABLE IF NOT EXISTS `Citizen` (
-  `cID` int(11) NOT NULL AUTO_INCREMENT,
+  `cID` int(11) NOT NULL,
   `f_name` varchar(30) NOT NULL,
   `l_name` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mob` varchar(15) NOT NULL,
+  `dob` date NOT NULL,
   `address_line1` varchar(50) NOT NULL,
   `address_line2` varchar(50) DEFAULT NULL,
   `city` varchar(30) NOT NULL,
   `district` varchar(30) NOT NULL,
   `state` varchar(30) NOT NULL,
   `pin_code` varchar(7) NOT NULL,
+  `pp_path` varchar(30) DEFAULT NULL,
   `citizen_govt_id` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`cID`,`email`),
-  UNIQUE KEY `mob` (`mob`,`citizen_govt_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores information about citizen' AUTO_INCREMENT=1 ;
+  UNIQUE KEY `mob` (`citizen_govt_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores information about citizen';
+
+--
+-- Dumping data for table `Citizen`
+--
+
+INSERT INTO `Citizen` (`cID`, `f_name`, `l_name`, `email`, `mob`, `dob`, `address_line1`, `address_line2`, `city`, `district`, `state`, `pin_code`, `pp_path`, `citizen_govt_id`) VALUES
+(2, 'Aravind', 'Ashok', 'aravindasokcn@gmail.com', '8374318559', '1995-02-05', 'boys hostel', 'iiit', 'Sri City', 'Chittoor', 'Andhra Pradesh', '517588', NULL, '');
 
 -- --------------------------------------------------------
 
@@ -55,10 +62,29 @@ CREATE TABLE IF NOT EXISTS `Citizen_reg` (
   `cID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `date_reg` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_reg` datetime NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cID`,`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `Citizen_reg`
+--
+
+INSERT INTO `Citizen_reg` (`cID`, `email`, `password`, `date_reg`, `active`) VALUES
+(2, 'aravindasokcn@gmail.com', 'b691c96a0e0e2674df4943221d5b4767', '2015-12-02 16:47:39', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Citizen_voted_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `Citizen_voted_comment` (
+  `comment_id` int(11) NOT NULL,
+  `pID` int(11) NOT NULL,
+  `cID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `Citizen_voted_problem` (
 --
 
 CREATE TABLE IF NOT EXISTS `Govt` (
-  `gID` int(11) NOT NULL AUTO_INCREMENT,
+  `gID` int(11) NOT NULL,
   `dep_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `contact_num` varchar(15) NOT NULL,
@@ -88,7 +114,14 @@ CREATE TABLE IF NOT EXISTS `Govt` (
   `state` varchar(30) NOT NULL,
   `pin_code` varchar(7) NOT NULL,
   PRIMARY KEY (`gID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Govt`
+--
+
+INSERT INTO `Govt` (`gID`, `dep_name`, `email`, `contact_num`, `city`, `district`, `state`, `pin_code`) VALUES
+(2, 'Water', 'water.chittoor@mail.com', '123456', 'Sri City', 'Chittoor', 'Andhra Pradesh', '517588');
 
 -- --------------------------------------------------------
 
@@ -100,10 +133,17 @@ CREATE TABLE IF NOT EXISTS `Govt_reg` (
   `gID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `date_registered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_registered` datetime NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`gID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `Govt_reg`
+--
+
+INSERT INTO `Govt_reg` (`gID`, `email`, `password`, `date_registered`, `active`) VALUES
+(2, 'water.chittoor@mail.com', '94978e53e0b8829141aebc78bc917fc8', '2015-12-02 18:07:08', 1);
 
 -- --------------------------------------------------------
 
@@ -121,6 +161,18 @@ CREATE TABLE IF NOT EXISTS `Govt_response_media` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Govt_voted_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `Govt_voted_comment` (
+  `comment_id` int(11) NOT NULL,
+  `pID` int(11) NOT NULL,
+  `gID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Problem`
 --
 
@@ -133,8 +185,8 @@ CREATE TABLE IF NOT EXISTS `Problem` (
   `city` varchar(30) DEFAULT NULL,
   `district` varchar(30) NOT NULL,
   `state` varchar(30) NOT NULL,
-  `pin_code` varchar(7) DEFAULT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `pin_code` varchar(7) NOT NULL,
+  `date_created` datetime NOT NULL,
   `votes` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -175,7 +227,7 @@ CREATE TABLE IF NOT EXISTS `Problem_media` (
 
 CREATE TABLE IF NOT EXISTS `Problem_notified` (
   `pID` int(11) NOT NULL,
-  `date_notified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_notified` datetime NOT NULL,
   PRIMARY KEY (`pID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -188,7 +240,7 @@ CREATE TABLE IF NOT EXISTS `Problem_notified` (
 CREATE TABLE IF NOT EXISTS `Problem_responded` (
   `pID` int(11) NOT NULL,
   `response` varchar(200) NOT NULL,
-  `date_responded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date_responded` datetime NOT NULL,
   `likes` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`pID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -201,9 +253,31 @@ CREATE TABLE IF NOT EXISTS `Problem_responded` (
 
 CREATE TABLE IF NOT EXISTS `Problem_solved` (
   `pID` int(11) NOT NULL,
-  `date_solved` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `verified_by_citizen` tinyint(1) NOT NULL DEFAULT '0',
+  `date_verified_by_citizen` datetime DEFAULT NULL,
   `verified_by_govt` tinyint(1) NOT NULL DEFAULT '0',
+  `date_verified_by_govt` datetime DEFAULT NULL,
+  `solved` tinyint(1) NOT NULL DEFAULT '0',
+  `date_solved` datetime DEFAULT NULL,
+  PRIMARY KEY (`pID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Problem_status`
+--
+
+CREATE TABLE IF NOT EXISTS `Problem_status` (
+  `pID` int(11) NOT NULL,
+  `status` varchar(30) NOT NULL COMMENT 'created, notified, taken_up, declined, notified_pincode, notified_local, solved',
+  `date_created` datetime NOT NULL,
+  `date_notified` datetime DEFAULT NULL,
+  `date_taken_up` datetime DEFAULT NULL,
+  `date_declined` datetime DEFAULT NULL,
+  `date_notified_pincode` datetime DEFAULT NULL,
+  `date_notified_local` datetime DEFAULT NULL,
+  `date_solved` datetime DEFAULT NULL,
   PRIMARY KEY (`pID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
